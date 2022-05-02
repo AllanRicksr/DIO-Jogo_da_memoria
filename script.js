d69a840b222f192548783791aa4c168312b723d8
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-05-02 14:51:14 -0400

    iniciando javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+let order = [];
+let clickedOrder = [];
+let ecore = 0;
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order) {
+        let elementColor = createColorElement(order[i]);
+    }
+}
